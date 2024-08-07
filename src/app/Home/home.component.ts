6a635f3e05f9c8f76b7de6f2f251552bc4728922
Andrew <aleclair.1105@outlook.com>
2024-08-07 09:02:43 -0500

    getting started

---
+++ src/app/Home/home.component.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'bot-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
+  })
+  export class homeComponent {
+    constructor() {}
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { homeComponent } from './Home/home.component';
+import { SiteHeaderComponent } from '_course-resources/site-header/site-header.component';
+import { catalogComponent } from './catalog copy/catalog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    homeComponent,
+    SiteHeaderComponent,
+    catalogComponent
+
   ],
   imports: [
     BrowserModule
